f9f6160517889a67d37d2749c17632c44b3d79ba
lyes <lyes.makhloufi@outlook.fr>
2020-06-20 08:58:35 +0200

    Restructuration

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.xml.sax.SAXException;
+
+
+
+public class Main {
+	
+
+	
+	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
+
+		
+		
+
+       
+       
+
+	}
+
+}
